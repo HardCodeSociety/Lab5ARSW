0b9b4c02c40681889976f6347b25bc2545ebb1fd
David Vaca <spm1dh@gmil.com>
2018-04-18 09:48:36 -0500

    Parte 2 nuevo laboratorio Finalizada

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintApplication.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintApplication.java
@@ -1,14 +1,17 @@
 package edu.eci.arsw.collabpaint;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.arsw.collabpaint"})
-public class CollabPaintApplication {
 
+
+public class CollabPaintApplication {        
 	public static void main(String[] args) {
 		SpringApplication.run(CollabPaintApplication.class, args);
 	}
 }
+
